e3550f9b2f0ce2fa8f054181fe7b4a273d41a89a
Pau Espin Pedrol <pespin@sysmocom.de>
2025-12-04 17:42:41 +0100

    Only export needed wrapper API
    Otherwise the asn_* symbols collide with other generated skeletons on
    apps/library clients (eg. libasn1c.git).
    
    Related: OS#6895
    Related: OS#6896
    Change-Id: I6d97ac94be91064be7203b11cffa4de9056afce1

---
+++ include/osmocom/tcap/tcap.h
@@ -0,0 +1,11 @@
+/* libosmo-asn1-tcap public API */
+#pragma once
+
+#include <stdint.h>
+#include <unistd.h>
+
+#include <osmocom/tcap/TCAP_TCMessage.h>
+
+int osmo_asn1_tcap_decode(struct TCAP_TCMessage *tcapmsg, const uint8_t *data, size_t data_len);
+
+void osmo_asn1_tcap_TCMessage_free_contents(struct TCAP_TCMessage *tcapmsg);
---
+++ src/tcap.c
@@ -0,0 +1,45 @@
+/* libosmo-asn1-tcap public API */
+/* (C) 2025 by sysmocom s.f.m.c. GmbH <info@sysmocom.de>
+ * All Rights Reserved
+ *
+ * SPDX-License-Identifier: GPL-2.0+
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include <stdint.h>
+#include <unistd.h>
+#include <errno.h>
+
+#include <osmocom/tcap/asn_codecs.h>
+#include <osmocom/tcap/TCAP_TCMessage.h>
+
+int osmo_asn1_tcap_decode(struct TCAP_TCMessage *tcapmsg, const uint8_t *data, size_t data_len)
+{
+
+	asn_dec_rval_t asn_rc;
+
+	memset(tcapmsg, 0, sizeof(struct TCAP_TCMessage));
+
+	asn_rc = ber_decode(0, &asn_DEF_TCAP_TCMessage, (void **)&tcapmsg, data, data_len);
+	if (asn_rc.code != RC_OK)
+		return -EINVAL;
+	return 0;
+}
+
+void osmo_asn1_tcap_TCMessage_free_contents(struct TCAP_TCMessage *tcapmsg)
+{
+	ASN_STRUCT_FREE_CONTENTS_ONLY(asn_DEF_TCAP_TCMessage, tcapmsg);
+}
